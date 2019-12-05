f00b69a168b1112b858baf85aee4f1c8374ba9b7
Paulo H. Santos <paulo.santos@citsamazonas.org.br>
2019-12-05 14:03:48 -0400

    PanelBottom built with placeholder design and working with the table

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { HashRouter as Router } from 'react-router-dom';
 
+import PanelBottom from '../scenes/general/panelBottom';
 import Main from '../scenes/main/';
 
 const Routes = () => (
   <Router>
     <div>
       <Route exact path="/" component={Main} />
+      <Route exact path="/panelbottom" component={PanelBottom} />
     </div>
   </Router>
 );

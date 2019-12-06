03ec8e3d0011ae283c170f95fecff5750d9c16d4
Erivan Souza da Silva Filho <erivan.filho@citsamazonas.local>
2019-12-06 14:13:42 -0400

    upperBar created

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { HashRouter as Router } from 'react-router-dom';
 
-import PanelBottom from '../scenes/general/panelBottom';
-import Main from '../scenes/main/';
+import UpperBar from '../scenes/general/upperBar/example';
 
 const Routes = () => (
   <Router>
     <div>
-      <Route exact path="/" component={Main} />
-      <Route exact path="/panelbottom" component={PanelBottom} />
+      <Route exact path="/" component={UpperBar} />
     </div>
   </Router>
 );

c65190027dd87dbe3d2a490d06f4ce054dc8cd78
Erivan Souza da Silva Filho <erivan.filho@citsamazonas.local>
2020-01-03 14:37:31 -0400

    monster poc was created

--- src/scenes/ar/Ar.js
+++ src/scenes/ar/Ar.js
@@ -1,73 +1,119 @@
 import React, { useEffect } from 'react';
-import { chave } from '../assets/';
+import oculos from '../assets/markers/oculos.patt';
 import sphere from '../assets/objects/sphere.glb';
 import monster from '../assets/objects/monster.glb';
-import oculos from '../assets/markers/oculos.patt';
+import duck from '../assets/objects/duck.glb';
+import intro from '../assets/objects/task.jpg';
+import complete from '../assets/objects/complete.jpg';
+import sound1 from '../assets/objects/dsbgdth1.mp3';
+import sound2 from '../assets/objects/dsbgsit2.mp3';
+import doom from '../assets/objects/doom.mp3';
 
 const Ar = () => {
-  const value = 0.5; //sphere
-  const valueM = 0.01; //sphere
+  const valueM = 0.02;
+  const valueSM = 0.01;
+  const valueD = 0.1;
+  let value = true;
+  const audio1 = new Audio(sound1);
+  const audio2 = new Audio(sound2);
+  const doomSoundtrack = new Audio(doom);
 
-  // useEffect(() => {
-  //   const pos = document
-  //     .querySelector('#objmk')
-  //     .getAttribute('position');
-  //   const myEl = document.querySelector('#boxinha');
+  useEffect(() => {
+    const pos = document
+      .querySelector('#objmk')
+      .getAttribute('position');
+    const intro = document.querySelector('#introScene');
+    const monsterScene = document.querySelector('#monsterScene');
+    const duckScene = document.querySelector('#duckScene');
+    const completeScene = document.querySelector('#completeScene');
+    const mkMonster = document.querySelector('#mkMonster');
+    doomSoundtrack.play();
+    doomSoundtrack.volume = 0.3;
 
-  //   const intervalId = setInterval(() => {
-  //     // console.log('pos:', pos);
+    setTimeout(() => {
+      intro.setAttribute('visible', 'false');
+    }, 5000);
 
-  //     if (pos.x < -2 && pos.x < -1.5) {
-  //       myEl.setAttribute('visible', 'true');
-  //     } else {
-  //       myEl.setAttribute('visible', 'false');
-  //     }
-  //   }, 1000);
+    const intervalId = setInterval(() => {
+      if (pos.x > 0.7 && pos.y > 0.8) {
+        doomSoundtrack.pause();
+        monsterScene.setAttribute('visible', 'true');
+        completeScene.setAttribute('visible', 'true');
+        duckScene.setAttribute('visible', 'false');
+        mkMonster.setAttribute('visible', 'false');
+        if (value) {
+          audio2.play();
+          value = false;
+        }
+      }
+    }, 1000);
 
-  //   return () => {
-  //     clearInterval(intervalId);
-  //   };
-  // }, []);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   return (
-    <a-scene
-      embedded
-      arjs="sourceType: webcam; debugUIEnabled: false;"
-    >
+    <a-scene arjs="sourceType: webcam; debugUIEnabled: false;">
       <a-assets>
-        <a-asset-item id="gltf" src={sphere}></a-asset-item>
+        <a-asset-item id="sphere" src={sphere}></a-asset-item>
         <a-asset-item id="monster" src={monster}></a-asset-item>
-        <img id="my-image" src={chave} />
+        <a-asset-item id="duck" src={duck}></a-asset-item>
+
+        <img id="intro" src={intro} />
+        <img id="complete" src={complete} />
       </a-assets>
 
       <a-marker id="objmk" type="pattern" url={oculos}>
         <a-gltf-model
-          src="#gltf"
-          position="0 0 0"
-          rotation="90 90 90"
-          scale={`${value} ${value} ${value}`}
+          id="mkMonster"
+          visible="true"
+          src="#monster"
+          position="0 2 0"
+          rotation="30 40 0"
+          animation-mixer="clip: *"
+          scale={`${valueM} ${valueM} ${valueM}`}
         ></a-gltf-model>
       </a-marker>
       <a-entity camera></a-entity>
 
       <a-gltf-model
-        src="#monster"
-        position="0 0.5 1"
-        rotation="30 40 0"
-        animation-mixer="clip: *"
-        scale={`${valueM} ${valueM} ${valueM}`}
+        id="duckScene"
+        src="#duck"
+        visible="true"
+        position="0.7 0.8 0"
+        rotation="0 -90 -45"
+        scale={`${valueD} ${valueD} ${valueD}`}
       ></a-gltf-model>
 
-      <a-box
-        id="boxinha"
-        position="2 0.5 -3"
-        rotation="0 45 45"
+      <a-image
+        id="completeScene"
+        src="#complete"
         visible="false"
-        scale="0.5 0.5 0.5"
-        color="#4CC3D9"
-      ></a-box>
+        position="0.2 0.8 0"
+        width="0.5"
+        height="0.2"
+      ></a-image>
 
-      <a-entity position="0 -1 2">
+      <a-gltf-model
+        id="monsterScene"
+        visible="false"
+        src="#monster"
+        position="0.7 0.8 0"
+        rotation="45 0 0"
+        scale={`${valueSM} ${valueSM} ${valueSM}`}
+      ></a-gltf-model>
+
+      <a-image
+        id="introScene"
+        src="#intro"
+        visible="true"
+        position="0 0.21 0"
+        width="1"
+        height="0.3"
+      ></a-image>
+
+      <a-entity camera position="0 -1 2">
         <a-camera></a-camera>
       </a-entity>
     </a-scene>
